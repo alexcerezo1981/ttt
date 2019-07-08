1444865772eedc8c1c26c125bfc47431f6dd6484
AlejandroCerezo <alex.cerezo@fabberz.com>
2019-07-08 17:13:52 -0400

    First Unit Testing

--- Tic.js
+++ Tic.js
@@ -13,7 +13,6 @@ for (x=1; x<10;x++){
 }
 
 var turn = "X"          // Variable to control the turn
-//var i=0               // Variable for the loop for
 var TurnCounter=0       // Variable to control when we got to the maximum of turns
 var WrongKey =true      // Variable to control if the press a wrong key
 var BoxUsed = false     // Variable to control if the box is already used
@@ -22,7 +21,6 @@ var BoxUsed = false     // Variable to control if the box is already used
 
     
 // Function to Display the board
-
 function DisplayBoard() {
     console.clear ()
     console.log ("Tic Tac Toe")
@@ -35,41 +33,28 @@ function DisplayBoard() {
     console.log(" " + BoardGame[6] + " | " + BoardGame[7] + " | " + BoardGame[8])
     console.log("--- --- ---")
     console.log ("")
+}
 
-    // If we don't have a winner yet, check if all the boxes are used ("Drawn"), if not display the turn of the next player
-    if (TurnCounter===9){
-        console.log ("DRAW!")
-        process.exit();
-    }else{
-        if (BoxUsed===false)
-        console.log ("it's the turn for " + turn + ". Press one of the numbers displayed on the board. Press 'e' for Exit")
-    }}
+function CheckBoxUsed(BoxUsed){
+    if (BoardGame[i-1]!=BoxUsed)
+        return BoxUsed = true
+    else
+        return BoxUsed = false
+}
 
 function CheckIfWinner(){
-    if (BoardGame[0]===BoardGame[1] && BoardGame[1]===BoardGame[2]){
-        console.log (BoardGame[0] + " WINS THE GAME!!!")
+
+    if (BoardGame[0]===BoardGame[1] && BoardGame[1]===BoardGame[2] || BoardGame[3]===BoardGame[4] && BoardGame[4]===BoardGame[5] || BoardGame[6]===BoardGame[7] && BoardGame[7]===BoardGame[8] || BoardGame[0]===BoardGame[3] && BoardGame[3]===BoardGame[6] || BoardGame[1]===BoardGame[4] && BoardGame[4]===BoardGame[7] || BoardGame[2]===BoardGame[5] && BoardGame[5]===BoardGame[8] || BoardGame[0]===BoardGame[4] && BoardGame[4]===BoardGame[8] || BoardGame[4]===BoardGame[6] && BoardGame[6]===BoardGame[2])  
+    {
+        console.log (turn + " WINS THE GAME!!!")
         process.exit()
-    }else if (BoardGame[3]===BoardGame[4] && BoardGame[4]===BoardGame[5]){
-        console.log (BoardGame[3] + " WINS THE GAME!!!")
-        process.exit()
-    }else if (BoardGame[6]===BoardGame[7] && BoardGame[7]===BoardGame[8]){
-        console.log (BoardGame[6] + " WINS THE GAME!!!")
-        process.exit()
-    }else if (BoardGame[0]===BoardGame[3] && BoardGame[3]===BoardGame[6]){
-        console.log (BoardGame[0] + " WINS THE GAME!!!")
-        process.exit()
-    }else if (BoardGame[1]===BoardGame[4] && BoardGame[4]===BoardGame[7]){
-        console.log (BoardGame[1] + " WINS THE GAME!!!")
-        process.exit()
-    }else if (BoardGame[2]===BoardGame[5] && BoardGame[5]===BoardGame[8]){
-        console.log (BoardGame[2] + " WINS THE GAME!!!")
-        process.exit()
-    }else if (BoardGame[0]===BoardGame[4] && BoardGame[4]===BoardGame[8]){
-        console.log (BoardGame[0] + " WINS THE GAME!!!")
-        process.exit()
-    }else if (BoardGame[4]===BoardGame[6] && BoardGame[6]===BoardGame[2]){
-        console.log (BoardGame[4] + " WINS THE GAME!!!")
-        process.exit()
+    }else{
+        if (turn==="X"){
+            turn="O"
+        }else{
+            turn="X"
+        }
+        TurnCounter=TurnCounter+1 
     }
 }
 
@@ -85,30 +70,31 @@ process.stdin.on('keypress', (str, key) => {
         process.exit();
     }
 
-    // We checked if the pressed key is a number between 0 and 8
+    // We checked if the pressed key is a number between 1 and 9
     for (i=1;i<10;i++){
         if(key.sequence === i.toString ()) {
 
             // Check if the box is already in use
-            if (BoardGame[i-1]!=i.toString ()){
+            BoxUsed=CheckBoxUsed(i.toString())
+            if (BoxUsed=false){
+            //if (BoardGame[i-1]!=i.toString ()){
                 console.log ("This box is already in used, please select another one")
                 BoxUsed = true
             }else{
                 BoardGame[i-1]=turn         // Update the array with the new value
-                
-                // Change the turn
-                if (turn==="X"){
-                    turn="O"
-                }else{
-                    turn="X"
-                }
-                TurnCounter=TurnCounter+1    
-                
-                // Update the board
-                DisplayBoard()
-
+ 
                 // Check if there is a winner
                 CheckIfWinner() 
+
+                // Update the board
+                DisplayBoard()
+                
+                // If we don't have a winner yet, check if all the boxes are used ("Drawn"), if not display the turn of the next player
+                if (TurnCounter===9){
+                    console.log ("DRAW!")
+                    process.exit();        
+                }else if (BoxUsed===false)
+                    console.log ("it's the turn for " + turn + ". Press one of the numbers displayed on the board. Press 'e' for Exit")
             }
             WrongKey=false
         }
